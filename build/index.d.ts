5ea619b12809eebc894c85899f0f1cfa751fb3bc
Artem Kholodenko <artem.kholodenko@instacart.com>
2019-11-06 16:12:11 -0800

    more scaffolding

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,1 +1,7 @@
-export declare function testMethod(param: string): string;
+import React from "react";
+export declare type Props = {
+    text: string;
+};
+export default class ExampleComponent extends React.Component<Props> {
+    render(): JSX.Element;
+}
